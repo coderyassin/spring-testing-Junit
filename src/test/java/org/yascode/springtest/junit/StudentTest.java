1e20d192228721335957bab73f6143c8cd20f82e
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2023-01-11 18:23:51 +0100

    BeforeEach & BeforeAll annotations

--- src/test/java/org/yascode/springtest/junit/StudentTest.java
+++ src/test/java/org/yascode/springtest/junit/StudentTest.java
@@ -1,23 +1,37 @@
 package org.yascode.springtest.junit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
+    private static Student student;
+    private static int i = 0;
+
+    @BeforeAll
+    public static void instantiateStudent() {
+        student = new Student();
+        student.setName("Yassin");
+        student.setAge(26);
+        System.out.println("The student has successfully instantiated");
+    }
+
+    @BeforeEach
+    public void runEveryCall() {
+        System.out.println("Execution number: " + ++i);
+    }
+
     @Test
     public void getName() {
-        Student student = new Student();
-        student.setName("Yassin");
         Assertions.assertEquals(student.getName(), "Yassin");
     }
 
     @Test
     public void getAge() {
-        Student student = new Student();
-        student.setAge(26);
         Assertions.assertEquals(student.getAge(), 26);
     }
 
